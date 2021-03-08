be61cb51c02850723f4a190b251b672d8ac0acf3
fthompson24 <fraserjthompson@gmail.com>
2021-03-08 00:12:21 -0600

    view note

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
 const path = require('path');
-
 const fs = require('fs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", function(req, res){
-    res.sendFile("./public/index.html")
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
-app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
 
-app.get("/api/notes", function(req, res){
-    let allSavedNotes = fs.readFileSync(path.join(__dirname, "./db/db.json"))
-    allSavedNotes = JSON.parse(allSavedNotes)
-    res.json(allSavedNotes)
-})
+app.get("/api/notes",(req, res) => {
+  let savedNotes = fs.readFileSync(path.join(__dirname, "./db/db.json"))
+  savedNotes = JSON.parse(savedNotes)
+  res.json(savedNotes)
+});
 
-app.post("/api/notes", function(req, res){
-    let allSavedNotes = fs.readFileSync(path.join(__dirname, "./db/db.json"))
-    allSavedNotes = JSON.parse(allSavedNotes)
-    allSavedNotes.push(req.body)
-    allSavedNotes = JSON.stringify(allSavedNotes)
-    fs.writeFileSync(path.join(__dirname, "./db/db.json"),allSavedNotes)
-    allSavedNotes = JSON.parse(allSavedNotes)
-    res.json(allSavedNotes)
-})
+app.post("/api/notes",(req, res) => {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+  let noteID = (savedNotes.length).toString();
+  let newNote = req.body;
+  newNote.id = noteID;
+  savedNotes.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+});
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server is listening on port ${PORT}!`);
 });
